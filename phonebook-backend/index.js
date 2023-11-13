bfec0a3b618286e13eb41f41d6b0da379aa9f1fb
lone-ion <stilos@gmail.com>
2023-11-13 17:36:50 +0200

    finish ex-3.2: Phonebook backend step2

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -24,8 +24,15 @@ let persons = [
    }
 ]
 
+
 app.get('/', (request, response) => {
    response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/info', (request, response) => {
+   const people = persons.length;
+   const datetime = new Date();
+   response.send(`<p>Phonebook has info for ${people} people</p><p>${datetime}</p`)
 })
 
 app.get('/api/persons', (request, response) => {
