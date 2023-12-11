e720dc25eb7d112af1ff0861cc57a79fe8a026cb
lone-ion <stilos@gmail.com>
2023-12-11 10:19:20 +0200

    finish ex-3.20*: Phonebook database, step8

--- phonebook-backend/models/person.js
+++ phonebook-backend/models/person.js
@@ -14,7 +14,15 @@ const recordSchema = new mongoose.Schema({
       type: String,
       minLength: 3
    },
-   number: String,
+   number: {
+      type: String,
+      validate: {
+         validator: function (v) {
+            return /^\d{2,3}-\d{1,}$/.test(v);
+         },
+         message: props => `${props.value} is not a valid phone number!`
+      },
+   },
 })
 
 recordSchema.set('toJSON', {
